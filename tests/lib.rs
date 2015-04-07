853d8b2d20f61b6c0a79927911b55e3ddcadc75a
Jason Ozias <jason.g.ozias@gmail.com>
2015-04-07 19:50:01 -0400

    bitflags to control auto-gen'd functions

--- tests/lib.rs
+++ tests/lib.rs
@@ -3,13 +3,15 @@ extern crate vergen;
 
 use std::env;
 use std::io::prelude::*;
-use vergen::vergen;
+use vergen::*;
 
 #[test]
 fn test_vergen() {
     let tmp = env::temp_dir();
     env::set_var("OUT_DIR",&tmp);
     env::set_var("TARGET", "x86_64-unknown-linux-gnu");
-    vergen();
+    let mut flags = Flags::all();
+    flags.toggle(NOW);
+    vergen(flags);
     assert!(&tmp.join("version.rs").exists());
 }
