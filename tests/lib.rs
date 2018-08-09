209298535d358f1f452f979835af93c656fd4556
Jason Ozias <jason.g.ozias@gmail.com>
2018-08-09 13:13:13 -0400

    Refactor into constants, update to latest rust conventions

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,23 +2,24 @@ extern crate vergen;
 
 use std::env;
 use std::fs::File;
-use vergen::*;
+use vergen::{vergen, ConstantsFlags, COMPILE_TIME};
 
 #[test]
 fn test_vergen() {
     let tmp = env::temp_dir();
     env::set_var("OUT_DIR", &tmp);
     env::set_var("TARGET", "x86_64-unknown-linux-gnu");
-    let mut flags = OutputFns::all();
-    flags.toggle(NOW);
-    assert!(vergen(flags).is_ok());
+    let mut flags = ConstantsFlags::all();
+    flags.toggle(COMPILE_TIME);
+    match vergen(flags) {
+        Ok(_) => assert!(true),
+        Err(e) => assert!(false, format!("{}", e)),
+    }
     match File::open(&tmp.join("version.rs")) {
-        Ok(ref mut f) => {
-            match f.metadata() {
-                Ok(meta) => assert!(meta.is_file()),
-                Err(_) => assert!(false),
-            }
-        }
+        Ok(ref mut f) => match f.metadata() {
+            Ok(meta) => assert!(meta.is_file()),
+            Err(_) => assert!(false),
+        },
         Err(_) => assert!(false),
     }
 }
