fb33c930eaa2fcea53290d8ea62b45405b2052d6
Jason Ozias <jason.ozias@kroger.com>
2018-10-16 08:20:06 -0400

    Handle metadata error more gracefully

--- src/output/envvar.rs
+++ src/output/envvar.rs
@@ -42,64 +42,66 @@ pub fn generate_cargo_keys(flags: ConstantsFlags) -> Fallible<()> {
     }
 
     let git_dir_or_file = PathBuf::from(".git");
-    let metadata = fs::metadata(&git_dir_or_file)?;
+    if let Ok(metadata) = fs::metadata(&git_dir_or_file) {
+        if metadata.is_dir() {
+            // Echo the HEAD path
+            let git_head_path = git_dir_or_file.join("HEAD");
+            println!("cargo:rerun-if-changed={}", git_head_path.display());
 
-    if metadata.is_dir() {
-        // Echo the HEAD path
-        let git_head_path = git_dir_or_file.join("HEAD");
-        println!("cargo:rerun-if-changed={}", git_head_path.display());
+            // Determine where HEAD points and echo that path also.
+            let mut f = File::open(&git_head_path)?;
+            let mut git_head_contents = String::new();
+            let _ = f.read_to_string(&mut git_head_contents)?;
+            eprintln!("HEAD contents: {}", git_head_contents);
+            let ref_vec: Vec<&str> = git_head_contents.split(": ").collect();
 
-        // Determine where HEAD points and echo that path also.
-        let mut f = File::open(&git_head_path)?;
-        let mut git_head_contents = String::new();
-        let _ = f.read_to_string(&mut git_head_contents)?;
-        eprintln!("HEAD contents: {}", git_head_contents);
-        let ref_vec: Vec<&str> = git_head_contents.split(": ").collect();
+            if ref_vec.len() == 2 {
+                let current_head_file = ref_vec[1];
+                let git_refs_path = PathBuf::from(".git").join(current_head_file);
+                println!("cargo:rerun-if-changed={}", git_refs_path.display());
+            } else {
+                eprintln!("You are most likely in a detached HEAD state");
+            }
+        } else if metadata.is_file() {
+            // We are in a worktree, so find out where the actual worktrees/<name>/HEAD file is.
+            let mut git_file = File::open(&git_dir_or_file)?;
+            let mut git_contents = String::new();
+            let _ = git_file.read_to_string(&mut git_contents)?;
+            let dir_vec: Vec<&str> = git_contents.split(": ").collect();
+            eprintln!(".git contents: {}", git_contents);
+            let git_path = dir_vec[1].trim();
 
-        if ref_vec.len() == 2 {
-            let current_head_file = ref_vec[1];
-            let git_refs_path = PathBuf::from(".git").join(current_head_file);
-            println!("cargo:rerun-if-changed={}", git_refs_path.display());
+            // Echo the HEAD psth
+            let git_head_path = PathBuf::from(git_path).join("HEAD");
+            println!("cargo:rerun-if-changed={}", git_head_path.display());
+
+            // Find out what the full path to the .git dir is.
+            let mut actual_git_dir = PathBuf::from(git_path);
+            actual_git_dir.pop();
+            actual_git_dir.pop();
+
+            // Determine where HEAD points and echo that path also.
+            let mut f = File::open(&git_head_path)?;
+            let mut git_head_contents = String::new();
+            let _ = f.read_to_string(&mut git_head_contents)?;
+            eprintln!("HEAD contents: {}", git_head_contents);
+            let ref_vec: Vec<&str> = git_head_contents.split(": ").collect();
+
+            if ref_vec.len() == 2 {
+                let current_head_file = ref_vec[1];
+                let git_refs_path = actual_git_dir.join(current_head_file);
+                println!("cargo:rerun-if-changed={}", git_refs_path.display());
+            } else {
+                eprintln!("You are most likely in a detached HEAD state");
+            }
         } else {
-            eprintln!("You are most likely in a detached HEAD state");
-        }
-    } else if metadata.is_file() {
-        // We are in a worktree, so find out where the actual worktrees/<name>/HEAD file is.
-        let mut git_file = File::open(&git_dir_or_file)?;
-        let mut git_contents = String::new();
-        let _ = git_file.read_to_string(&mut git_contents)?;
-        let dir_vec: Vec<&str> = git_contents.split(": ").collect();
-        eprintln!(".git contents: {}", git_contents);
-        let git_path = dir_vec[1].trim();
-
-        // Echo the HEAD psth
-        let git_head_path = PathBuf::from(git_path).join("HEAD");
-        println!("cargo:rerun-if-changed={}", git_head_path.display());
-
-        // Find out what the full path to the .git dir is.
-        let mut actual_git_dir = PathBuf::from(git_path);
-        actual_git_dir.pop();
-        actual_git_dir.pop();
-
-        // Determine where HEAD points and echo that path also.
-        let mut f = File::open(&git_head_path)?;
-        let mut git_head_contents = String::new();
-        let _ = f.read_to_string(&mut git_head_contents)?;
-        eprintln!("HEAD contents: {}", git_head_contents);
-        let ref_vec: Vec<&str> = git_head_contents.split(": ").collect();
-
-        if ref_vec.len() == 2 {
-            let current_head_file = ref_vec[1];
-            let git_refs_path = actual_git_dir.join(current_head_file);
-            println!("cargo:rerun-if-changed={}", git_refs_path.display());
-        } else {
-            eprintln!("You are most likely in a detached HEAD state");
-        }
+            return Err(failure::err_msg(
+                "Invalid .git format (Not a directory or a file)",
+            ));
+        };
     } else {
-        return Err(failure::err_msg(
-            "Invalid .git format (Not a directory or a file)",
-        ));
-    };
+        eprintln!("Unable to generate 'cargo:rerun-if-changed'");
+    }
 
     Ok(())
 }
