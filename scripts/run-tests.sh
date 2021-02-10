7d3365c61c507f49f954981c334983ffedc9cfa4
Jason Ozias <jason.ozias@kroger.com>
2021-02-09 19:34:28 -0500

    weird no tests when compiling on stable with features

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -5,11 +5,11 @@ cargo clean
 
 if [ "${TRAVIS_RUST_VERSION}" = "stable" ]; then
     cargo build
-    cargo test
+    cargo test  --all-features
 elif [ "${TRAVIS_RUST_VERSION}" = "beta" ]; then
     cargo build --features beta
-    cargo test --features beta
+    cargo test
 else
     cargo build --features nightly
-    cargo test --features nightly
+    cargo test
 fi
