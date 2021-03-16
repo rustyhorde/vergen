22a2a2640edf9fdc8dcaa936b375f124c8bad491
Jason Ozias <jason.g.ozias@gmail.com>
2021-03-16 17:04:55 -0400

    version bump for next release, docs, lints (#55)
    Co-authored-by: Jason Ozias <jason.ozias@kroger.com>

--- build.rs
+++ build.rs
@@ -9,6 +9,7 @@ pub fn main() {
     );
     println!("cargo:rustc-env=VERGEN_GIT_SEMVER=v3.2.0-86-g95fc0f5");
     nightly_lints();
+    beta_lints();
 }
 
 #[rustversion::nightly]
@@ -18,3 +19,11 @@ fn nightly_lints() {
 
 #[rustversion::not(nightly)]
 fn nightly_lints() {}
+
+#[rustversion::any(beta, nightly)]
+fn beta_lints() {
+    println!("cargo:rustc-cfg=beta_lints");
+}
+
+#[rustversion::stable]
+fn beta_lints() {}
