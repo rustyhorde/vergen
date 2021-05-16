044dd37e83fb5c9a1454d9ec666ac7a1da74e9e2
Jason Ozias <jason.g.ozias@gmail.com>
2021-05-16 12:18:36 -0400

    removed disjoint lint

--- src/lib.rs
+++ src/lib.rs
@@ -198,7 +198,6 @@
     dead_code,
     deprecated,
     deprecated_in_future,
-    disjoint_capture_drop_reorder,
     drop_bounds,
     elided_lifetimes_in_paths,
     ellipsis_inclusive_range_patterns,
