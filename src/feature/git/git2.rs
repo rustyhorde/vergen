e6f116462bf986b523aa5c7cb57b1b34272ddb5f
Jason Ozias <jason.g.ozias@gmail.com>
2022-12-31 14:30:31 -0500

    'git2' and 'gix' test coverage

--- src/feature/git/git2.rs
+++ src/feature/git/git2.rs
@@ -11,6 +11,8 @@ use crate::{
     key::VergenKey,
     utils::fns::{add_default_map_entry, add_map_entry},
 };
+#[cfg(test)]
+use anyhow::anyhow;
 use anyhow::{Error, Result};
 // remove these when impl complete
 use git2_rs as _;
@@ -34,8 +36,13 @@ pub(crate) struct Config {
     pub(crate) git_commit_timestamp: bool,
     // git describe --always (optionally --tags, --dirty)
     pub(crate) git_describe: bool,
+    git_describe_dirty: bool,
+    git_describe_tags: bool,
     // git rev-parse HEAD (optionally with --short)
     pub(crate) git_sha: bool,
+    git_sha_short: bool,
+    #[cfg(test)]
+    fail: bool,
 }
 
 /// The `VERGEN_GIT_*` configuration features
@@ -65,14 +72,124 @@ pub(crate) struct Config {
 /// ```
 #[cfg_attr(docsrs, doc(cfg(feature = "git")))]
 impl EmitBuilder {
-    /// Enable all of the `VERGEN_GIT_*` options
+    /// Emit all of the `VERGEN_GIT_*` instructions
     pub fn all_git(&mut self) -> &mut Self {
         self.git_branch()
+            .git_commit_author_email()
+            .git_commit_author_name()
+            .git_commit_count()
+            .git_commit_date()
+            .git_commit_message()
+            .git_commit_timestamp()
+            .git_describe(false, false)
+            .git_sha(false)
     }
 
-    /// Emit the git branch instruction
+    /// Emit the current git branch
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_BRANCH=<BRANCH_NAME>
+    /// ```
+    ///
     pub fn git_branch(&mut self) -> &mut Self {
         self.git_config.git_branch = true;
+        self
+    }
+
+    /// Emit the author email of the most recent commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_AUTHOR_EMAIL=<AUTHOR_EMAIL>
+    /// ```
+    ///
+    pub fn git_commit_author_email(&mut self) -> &mut Self {
+        self.git_config.git_commit_author_email = true;
+        self
+    }
+
+    /// Emit the author name of the most recent commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_AUTHOR_NAME=<AUTHOR_NAME>
+    /// ```
+    ///
+    pub fn git_commit_author_name(&mut self) -> &mut Self {
+        self.git_config.git_commit_author_name = true;
+        self
+    }
+
+    /// Emit the total commit count to HEAD
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_COUNT=<COUNT>
+    /// ```
+    ///
+    pub fn git_commit_count(&mut self) -> &mut Self {
+        self.git_config.git_commit_count = true;
+        self
+    }
+
+    /// Emit the commit date of the latest commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_DATE=<YYYY-MM-DD>
+    /// ```
+    ///
+    pub fn git_commit_date(&mut self) -> &mut Self {
+        self.git_config.git_commit_date = true;
+        self
+    }
+
+    /// Emit the commit message of the latest commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_MESSAGE=<MESSAGE>
+    /// ```
+    ///
+    pub fn git_commit_message(&mut self) -> &mut Self {
+        self.git_config.git_commit_message = true;
+        self
+    }
+
+    /// Emit the commit timestamp of the latest commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_COMMIT_TIMESTAMP=<YYYY-MM-DDThh:mm:ssZ>
+    /// ```
+    ///
+    pub fn git_commit_timestamp(&mut self) -> &mut Self {
+        self.git_config.git_commit_timestamp = true;
+        self
+    }
+
+    /// Emit the describe output
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_DESCRIBE=<DESCRIBE>
+    /// ```
+    ///
+    /// Optionally, add the `dirty` or `tags` flag to describe.
+    /// See [`git describe`](https://git-scm.com/docs/git-describe#_options) for more details
+    ///
+    pub fn git_describe(&mut self, dirty: bool, tags: bool) -> &mut Self {
+        self.git_config.git_describe = true;
+        self.git_config.git_describe_dirty = dirty;
+        self.git_config.git_describe_tags = tags;
+        self
+    }
+
+    /// Emit the SHA of the latest commit
+    ///
+    /// ```text
+    /// cargo:rustc-env=VERGEN_GIT_SHA=<SHA>
+    /// ```
+    ///
+    /// Optionally, add the `short` flag to rev-parse.
+    /// See [`git rev-parse`](https://git-scm.com/docs/git-rev-parse#_options_for_output) for more details.
+    ///
+    pub fn git_sha(&mut self, short: bool) -> &mut Self {
+        self.git_config.git_sha = true;
+        self.git_config.git_sha_short = short;
         self
     }
 
@@ -117,7 +234,31 @@ impl EmitBuilder {
         }
     }
 
+    #[cfg(not(test))]
     pub(crate) fn add_git_map_entries(
+        &self,
+        idempotent: bool,
+        map: &mut RustcEnvMap,
+        rerun_if_changed: &mut Vec<String>,
+    ) -> Result<()> {
+        self.inner_add_git_map_entries(idempotent, map, rerun_if_changed)
+    }
+
+    #[cfg(test)]
+    pub(crate) fn add_git_map_entries(
+        &self,
+        idempotent: bool,
+        map: &mut RustcEnvMap,
+        rerun_if_changed: &mut Vec<String>,
+    ) -> Result<()> {
+        if self.git_config.fail {
+            Err(anyhow!("failed to create entries"))
+        } else {
+            self.inner_add_git_map_entries(idempotent, map, rerun_if_changed)
+        }
+    }
+
+    fn inner_add_git_map_entries(
         &self,
         _idempotent: bool,
         map: &mut RustcEnvMap,
@@ -152,6 +293,30 @@ mod test {
         assert_eq!(1, config.cargo_rustc_env_map.len());
         assert_eq!(0, count_idempotent(config.cargo_rustc_env_map));
         assert_eq!(0, config.warnings.len());
+        Ok(())
+    }
+
+    #[test]
+    #[serial_test::parallel]
+    fn git_error_fails() -> Result<()> {
+        let mut config = EmitBuilder::builder();
+        let _ = config.fail_on_error();
+        let _ = config.all_git();
+        config.git_config.fail = true;
+        assert!(config.test_emit().is_err());
+        Ok(())
+    }
+
+    #[test]
+    #[serial_test::parallel]
+    fn git_error_defaults() -> Result<()> {
+        let mut config = EmitBuilder::builder();
+        let _ = config.all_git();
+        config.git_config.fail = true;
+        let emitter = config.test_emit()?;
+        assert_eq!(9, emitter.cargo_rustc_env_map.len());
+        assert_eq!(9, count_idempotent(emitter.cargo_rustc_env_map));
+        assert_eq!(9, emitter.warnings.len());
         Ok(())
     }
 }
