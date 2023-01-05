43f109359bc2bd8fca595ab9bca8704460f9becc
Jason Ozias <jason.g.ozias@gmail.com>
2023-01-05 09:29:14 -0500

    Add 'SOURCE_DATE_EPOCH' test

--- tests/build_output.rs
+++ tests/build_output.rs
@@ -3,6 +3,7 @@ mod test_build {
     use anyhow::Result;
     use lazy_static::lazy_static;
     use regex::Regex;
+    use std::env;
     use vergen::EmitBuilder;
 
     lazy_static! {
@@ -26,6 +27,13 @@ mod test_build {
 cargo:rustc-env=VERGEN_BUILD_TIMESTAMP=VERGEN_IDEMPOTENT_OUTPUT
 cargo:warning=VERGEN_BUILD_DATE set to default
 cargo:warning=VERGEN_BUILD_TIMESTAMP set to default
+cargo:rerun-if-changed=build.rs
+cargo:rerun-if-env-changed=VERGEN_IDEMPOTENT
+cargo:rerun-if-env-changed=SOURCE_DATE_EPOCH
+"#;
+
+    const SOURCE_DATE_EPOCH_IDEM_OUTPUT: &str = r#"cargo:rustc-env=VERGEN_BUILD_DATE=2022-12-23
+cargo:rustc-env=VERGEN_BUILD_TIMESTAMP=2022-12-23T15:29:20.000000000Z
 cargo:rerun-if-changed=build.rs
 cargo:rerun-if-env-changed=VERGEN_IDEMPOTENT
 cargo:rerun-if-env-changed=SOURCE_DATE_EPOCH
@@ -71,6 +79,20 @@ cargo:rerun-if-env-changed=SOURCE_DATE_EPOCH
             .emit_to(&mut stdout_buf)?;
         let output = String::from_utf8_lossy(&stdout_buf);
         assert_eq!(QUIET_IDEM_OUTPUT, output);
+        Ok(())
+    }
+
+    #[test]
+    #[serial_test::serial]
+    fn build_all_sde_output() -> Result<()> {
+        env::set_var("SOURCE_DATE_EPOCH", "1671809360");
+        let mut stdout_buf = vec![];
+        EmitBuilder::builder()
+            .all_build()
+            .emit_to(&mut stdout_buf)?;
+        let output = String::from_utf8_lossy(&stdout_buf);
+        assert_eq!(SOURCE_DATE_EPOCH_IDEM_OUTPUT, output);
+        env::remove_var("SOURCE_DATE_EPOCH");
         Ok(())
     }
 }
