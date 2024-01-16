54cff2e1373964a52056ea172a11d1dc9aee1842
Jason Ozias <jason.g.ozias@gmail.com>
2024-01-15 21:57:19 -0500

    Add cargo feature unification test

--- run_all.fish
+++ run_all.fish
@@ -9,6 +9,7 @@ cargo test-all-features; and \
 cargo test -p vergen -F build,cargo,git,gitcl,rustc,si; and \
 cargo test -p vergen -F build,cargo,git,git2,rustc,si; and \
 cargo test -p vergen -F build,cargo,git,gitoxide,rustc,si; and \
+cargo test -p vergen -F build,cargo,git,gitcl,git2,gitoxide,rustc,si; and \
 cargo test -p vergen-pretty -F __vergen_test; and \
 cargo test -p vergen-pretty -F __vergen_test,color; and \
 cargo test -p vergen-pretty -F __vergen_test,header; and \
