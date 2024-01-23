ee096f9c4db55d53eb0669c5178e808bcf0d3eae
Jason Ozias <jason.g.ozias@gmail.com>
2024-01-23 12:43:18 -0500

    Update main.yml to include all coverage tasks

--- run_all.fish
+++ run_all.fish
@@ -25,11 +25,17 @@ cargo test -p vergen-pretty -F __vergen_test,color,header,trace; and \
 cargo test -p vergen-pretty -F __vergen_test,header,serde,trace; and \
 cargo test -p vergen-pretty -F __vergen_test,color,header,serde,trace; and \
 cargo llvm-cov clean --workspace; and \
+cargo llvm-cov -p vergen-lib -F unstable --no-report; and \
 cargo llvm-cov -p vergen-lib -F unstable,build,cargo,git,rustc,si --no-report; and \
+cargo llvm-cov -p vergen -F unstable --no-report; and \
 cargo llvm-cov -p vergen -F unstable,build,cargo,rustc,si --no-report; and \
+cargo llvm-cov -p vergen-git2 -F unstable --no-report; and \
 cargo llvm-cov -p vergen-git2 -F unstable,build,cargo,rustc,si --no-report; and \
+cargo llvm-cov -p vergen-gitcl -F unstable --no-report; and \
 cargo llvm-cov -p vergen-gitcl -F unstable,build,cargo,rustc,si --no-report; and \
+cargo llvm-cov -p vergen-gix -F unstable --no-report; and \
 cargo llvm-cov -p vergen-gix -F unstable,build,cargo,rustc,si --no-report; and \
+cargo llvm-cov -p vergen-pretty -F __vergen_test --no-report; and \
 cargo llvm-cov -p vergen-pretty -F __vergen_test,color,header,serde,trace --no-report; and \
 cargo llvm-cov report --lcov --output-path lcov.info; and \
 cargo llvm-cov report --html; and \
