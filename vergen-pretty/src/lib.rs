9e752407e428ec5d58e8890aa9fa1d27526bb822
Jason Ozias <jason.g.ozias@gmail.com>
2024-03-07 12:43:26 -0500

    Added vergen-lib workflow (#325)
    * added vergen-lib workflow
    
    * added llvm-cov feature for nightly
    
    * lets try codecove again

--- vergen-pretty/src/lib.rs
+++ vergen-pretty/src/lib.rs
@@ -303,6 +303,7 @@ assert!(!buf.is_empty());
 )]
 #![cfg_attr(all(doc, nightly), feature(doc_auto_cfg))]
 #![cfg_attr(all(docsrs, nightly), feature(doc_cfg))]
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 
 #[cfg(feature = "header")]
 mod header;
