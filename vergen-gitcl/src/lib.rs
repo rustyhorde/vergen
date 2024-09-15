fad609655ad48a8445aece6add59fae06c0f5515
Jason Ozias <jason.g.ozias@gmail.com>
2024-09-15 13:25:47 -0400

    Fixed missing doc lint issue (#377)
    * Fixed missing doc lint issue
    
    * Removed lint that was converted to an error

--- vergen-gitcl/src/lib.rs
+++ vergen-gitcl/src/lib.rs
@@ -276,7 +276,6 @@ let build = BuildBuilder::default().build_timestamp(true).build()?;"##
         clashing_extern_declarations,
         coherence_leak_check,
         confusable_idents,
-        const_eval_mutable_ptr_in_final_value,
         const_evaluatable_unchecked,
         const_item_mutation,
         dead_code,
