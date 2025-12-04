fec2c8e5f32b42d41b741cd0cd4000b98cee3d9d
Jason Ozias <jason.g.ozias@gmail.com>
2025-12-04 12:51:43 -0500

    Removed references to new unknown lints (#466)

--- test_util/src/lib.rs
+++ test_util/src/lib.rs
@@ -273,8 +273,6 @@ the [`TestRepos`] struct to creat temporary git repositories useful for `vergen-
         multiple_supertrait_upcastable,
         must_not_suspend,
         non_exhaustive_omitted_patterns,
-        supertrait_item_shadowing_definition,
-        supertrait_item_shadowing_usage,
         unqualified_local_imports,
     )
 )]
