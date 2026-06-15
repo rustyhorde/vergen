a3ffa569e9d0ea5a151813128acb992c09eddffe
Jason Ozias <jason.g.ozias@gmail.com>
2026-06-15 09:50:05 -0400

    Fix parse deprecation issues (#505)

--- vergen-git2/src/git2/mod.rs
+++ vergen-git2/src/git2/mod.rs
@@ -832,7 +832,7 @@ impl Git2 {
                     cargo_warning,
                 );
             } else {
-                let format = format_description::parse("[year]-[month]-[day]")?;
+                let format = format_description::parse_borrowed::<1>("[year]-[month]-[day]")?;
                 add_map_entry(
                     VergenKey::GitCommitDate,
                     ts.format(&format)?,
